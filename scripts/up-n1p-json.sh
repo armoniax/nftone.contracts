be8a9e0af42d29eb8f31558cddbe5200323294e7
masteray <masteraychen@gmail.com>
2022-10-11 20:12:46 +0800

    add n1p json

--- scripts/up-n1p-json.sh
+++ scripts/up-n1p-json.sh
@@ -27,3 +27,11 @@ mpush pass.ntoken issue '["'$acct'",[20000,[10006,0]],""]' -p $acct
 
 
 # mpush pass.ntoken transfer '["nftone.fund","armoniaadmin",[[30, [1300, 0]]],"add:0"]' -p $acct
+
+# add amax.split plan
+mpush amax.split addplan '["pass.mart","6,MUSDT",false]' -p armoniaadmin
+### return plan_id = 1
+mpush amax.split setplan  '["pass.mart",1,[["kversokverso",200000],["nftinstitute",3100000]]]' -parmoniaadmin
+
+mpush pass.custody addplan '["pass.mart","NFTOne Pass #1","pass.ntoken",[10001, 0],365,1]' -p pass.mart
+mpush pass.mart addpass '["nftone.fund","NFTOne Pass #1",[10001, 0],"330.000000 MUSDT","2022-10-18T01:30:00","2022-11-18T01:30:00",1]' -p nftone.fund
